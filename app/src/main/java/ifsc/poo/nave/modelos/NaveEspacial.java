ce8e00484e5fefc226d300e28167e1d56482e0f4
ArthurOReis <arthuroreis2005@gmail.com>
2025-06-16 09:00:31 -0300

    Implementando classe abstrata Nave Espacial

--- app/src/main/java/ifsc/poo/nave/modelos/NaveEspacial.java
+++ app/src/main/java/ifsc/poo/nave/modelos/NaveEspacial.java
@@ -1,5 +1,21 @@
 package ifsc.poo.nave.modelos;
 
-public class NaveEspacial {
-    
+public abstract class NaveEspacial {
+    private int id;
+    private static int incrementaId = 0;
+    private int velocidadeAtual;
+
+    public NaveEspacial(){
+        this.id = incrementaId + 1;
+        this.velocidadeAtual = 0;
+    }
+
+    public abstract String acelerar();
+
+    public abstract String frear();
+
+    public abstract String decolar();
+
+    public abstract String pousar();
+
 }
